df7d2b4f1307eb1fb7c55e229e9b3a59a4e41b48
marco <you@example.com>
2025-01-04 16:17:05 +0100

    first config

---
+++ configuration.nix
@@ -0,0 +1,118 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+  };
+
+  networking = {
+    networkmanager.enable = true;
+    hostName = "acer-aspire"; # Define your hostname.
+    wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Rome";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+        LC_ADDRESS = "it_IT.UTF-8";
+        LC_IDENTIFICATION = "it_IT.UTF-8";
+        LC_MEASUREMENT = "it_IT.UTF-8";
+        LC_MONETARY = "it_IT.UTF-8";
+        LC_NAME = "it_IT.UTF-8";
+        LC_NUMERIC = "it_IT.UTF-8";
+        LC_PAPER = "it_IT.UTF-8";
+        LC_TELEPHONE = "it_IT.UTF-8";
+        LC_TIME = "it_IT.UTF-8";
+    };
+  };
+
+
+  # Enable the X11 windowing system.
+  services = {
+    xserver = {
+        enable = true;
+        # Enable the XFCE Desktop Environment.
+        desktopManager.xfce.enable = true;
+        # Configure keymap in X11
+        xkb = {
+            layout = "us";
+            variant = "";
+        };
+        displayManager.lightdm.enable = true;
+        displayManager.autoLogin.enable = false;
+        displayManager.autoLogin.user = "marco";
+    };
+    
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+    };
+  };
+  
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.marco = {
+    isNormalUser = true;
+    description = "marco";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  programs = {
+    firefox.enable = true
+  };
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    vim 
+    wget
+    git
+    vscode
+  ];
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "My nixOs configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: {
+    
+    nixosConfigurations.acer-aspire = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      # Set all inputs parameters as special arguments for all submodules,
+      # so you can directly use all dependencies in inputs in submodules
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
