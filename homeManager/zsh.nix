8fa8de4d3301022a166b43fe8bc9c53a0a41f849
marco <you@example.com>
2025-01-06 22:05:21 +0100

    improve zsh

--- homeManager/zsh.nix
+++ homeManager/zsh.nix
@@ -1,16 +1,48 @@
 { pkgs, ... }:
 
 {
+
+  home.packages = with pkgs; [];
+
+  programs.fzf.enable = true;
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
+    history = {
+      # Number of entries that are saved
+      save = 100000;
+      # Number of entries that are loaded
+      size = 100000;
+      ignoreAllDups = true;
+      expireDuplicatesFirst = true;
+    };
+
     shellAliases = {
       ll = "ls -l";
       update = "sudo nixos-rebuild switch";
     };
-    history.size = 10000;
+
+    initExtra = ''      
+      autoload -U edit-command-line
+      bindkey '^[[A' history-search-backward 
+      bindkey '^[[B' history-search-forward 
+    '';
+
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
   };
 }
