4b14016ad20079a625d714e893de24b508792f5e
Marco Ravazzini <marco.ravazzini@bruker.com>
2025-01-27 14:39:32 +0100

    better lf setup

--- homeManager/lf.nix
+++ homeManager/lf.nix
@@ -1,27 +1,47 @@
-{ config, lib, ... }:
+{ pkgs, config, lib, ... }:
 
 {
   options = {
     homeModules.lf.enable = lib.mkEnableOption "enable lf";
   };
   config = lib.mkIf config.homeModules.lf.enable {
-    # basic configuration of git
+  
+    xdg.configFile."lf/icons".source = ../assets/lf_icons;
+    xdg.configFile."lf/colors".source = ../assets/lf_colors;
+    
+    # Documentation: https://github.com/gokcehan/lf/blob/master/doc.md
     programs.lf = {
       enable = true;
 
+      commands = {
+        mkdir = ''
+        ''${{
+          printf "Directory Name: "
+          read DIR
+          mkdir $DIR
+        }}
+        '';
+      };
+
       keybindings= {
         gh = "cd ~";
+        c = "mkdir";
+        "<enter>" = "open";
+        i = ''''$${pkgs.bat}/bin/bat --paging=always --theme="Sublime Snazzy" "$f"'';
       };
 
       settings={
-        number = true;
+        number = false;
+        mouse = true;
         preview= true;
         hidden= true;
         drawbox= true;
+        dircounts=true;
         icons= true;
         ignorecase= true;
         ratios = [ 1 1 2 ];
         tabstop = 4;
+        info=[ "size" ];
       };
     };
   };
