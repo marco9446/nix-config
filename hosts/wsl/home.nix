3ee6b1dda075b17f49d21cfe510aa4f9c5781b0d
Marco Ravazzini <marco.ravazzini@bruker.com>
2025-02-28 09:35:12 +0100

    fix wsl

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -4,4 +4,17 @@
   imports = [
     ../../modules/homeManager
   ];
+
+  homeModules = {
+    git.enable = true;
+    starship.enable = true;
+    vsCodium.enable = false;
+    xfce.enable = false;
+    yt-dlp.enable = false;
+    zsh.enable = true;
+    eza.enable = true;
+    yazi.enable = true;
+    zoxide.enable = true;
+    gtk.enable = false;
+  };
 }
