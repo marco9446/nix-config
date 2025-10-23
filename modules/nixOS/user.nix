d9b2aedc354c056f9a5f9734f2a8e8ee8b492f86
Marco Ravazzini <marco.ravazzini@bruker.com>
2025-10-23 13:09:47 +0200

    add lenovo public key to list of authorized key

--- modules/nixOS/user.nix
+++ modules/nixOS/user.nix
@@ -14,6 +14,10 @@
       extraGroups = [ "networkmanager" "wheel" ];
       shell = config.modules.customConfig.userShell;
       initialPassword = "changeMe";
+
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVkOxsmgT2thGbGjDeqL8Bp/wxnxu1OQlqcFXLZ81h1 lenovo-x1"
+      ];
     };
   };
 }
