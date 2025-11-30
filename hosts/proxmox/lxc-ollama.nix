6778b6d9e3ec33038c6ab31a59f38be4c27f0783
marco <marco@test.com>
2025-11-30 16:40:48 +0100

    update lxc ollama

--- hosts/proxmox/lxc-ollama.nix
+++ hosts/proxmox/lxc-ollama.nix
@@ -1,4 +1,6 @@
 { pkgs, ... }: {
+  # you need to passthrough the GPU devices to the LXC container in proxmox ui
+  # /dev/dri/renderD128 and /dev/kfd
 
   imports = [
     ../../modules/defaultPveLxcConfig.nix
@@ -10,15 +12,20 @@
       rocmPackages.clr.icd
     ];
   };
-
-  modules = { };
+  # Add ollama user to render/video groups for GPU access
+  users.users.ollama = {
+    extraGroups = [ "render" "video" ];
+  };
 
   services.ollama = {
     enable = true;
+    user = "ollama";
     # Optional: preload models, see https://ollama.com/library
     loadModels = [ ];
     acceleration = "rocm";
-    rocmOverrideGfx = "11.0.0"; # Required for Radeon 890M (RDNA 3.5)
+    # Radeon 890M is RDNA 3.5. 
+    # If 11.0.0 is unstable, try "11.0.2"
+    rocmOverrideGfx = "11.0.0";
     openFirewall = true;
   };
 
