67d2b034da8eea6b2e2b8d76ea027a4fd8e46d39
marco <marco@test.com>
2025-12-30 13:22:51 +0100

    test nvidia on macbook

--- modules/nixOS/nvidia.nix
+++ modules/nixOS/nvidia.nix
@@ -1,4 +1,10 @@
-{ config, lib, ... }: {
+{
+  config,
+  lib,
+  host,
+  ...
+}:
+{
 
   options = {
     modules.nvidia.enable = lib.mkEnableOption "enable nvidia";
@@ -18,19 +24,19 @@
 
       # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
       # Enable this if you have graphical corruption issues or application crashes after waking
-      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
       # of just the bare essentials.
       powerManagement.enable = false;
 
       # Fine-grained power management. Turns off GPU when not in use.
       # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-      powerManagement.finegrained = true;
+      powerManagement.finegrained = host != "macbook";
 
       # Use the NVidia open source kernel module (not to be confused with the
       # independent third-party "nouveau" open source driver).
-      # Support is limited to the Turing and later architectures. Full list of 
-      # supported GPUs is at: 
-      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
       # Only available from driver 515.43.04+
       # Currently alpha-quality/buggy, so false is currently the recommended setting.
       open = false;
@@ -40,17 +46,26 @@
       nvidiaSettings = true;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package =
+        if host == "macbook" then
+          config.boot.kernelPackages.nvidiaPackages.legacy_470
+        else
+          config.boot.kernelPackages.nvidiaPackages.stable;
 
       prime = {
         # Make sure to use the correct Bus ID values for your system!
-        intelBusId = "PCI:2@0:0:0";
-        nvidiaBusId = "PCI:1@0:0:0";
+        intelBusId = if host == "macbook" then "PCI:0:2:0" else "PCI:2@0:0:0";
+        nvidiaBusId = if host == "macbook" then "PCI:1:0:0" else "PCI:1@0:0:0";
         offload = {
           enable = true;
           enableOffloadCmd = true; # Provides `nvidia-offload` command.
         };
       };
     };
+
+    boot.kernelParams = lib.mkIf (host == "macbook") [
+      "nvidia-drm.modeset=1"
+      "acpi_osi=Darwin"
+    ];
   };
 }
